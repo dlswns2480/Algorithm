f39a63ee139ce299a7ce9ea6f9e466cb86a1a2d8
최인준 <54973090+dlswns2480@users.noreply.github.com>
2024-04-05 14:24:46 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -4,27 +4,21 @@ class Solution {
         int[] answer = {};
         int sum = 0;
         int lt = 0;
-        List<int[]> lst = new ArrayList<>();
-        
+
+        int idxSize = Integer.MAX_VALUE;
         for(int rt = 0; rt < sequence.length; rt++){
             sum += sequence[rt];
-            if(sum == k){
-                lst.add(new int[]{lt, rt});
+            
+            if(sum == k && rt - lt < idxSize){
+                answer = new int[]{lt, rt};
+                idxSize = rt- lt;
             }
             while(sum >= k){
                 sum -= sequence[lt++];
-                if(sum == k) {
-                    lst.add(new int[]{lt, rt});
+                if(sum == k && rt - lt < idxSize){
+                    answer = new int[]{lt, rt};
+                    idxSize = rt- lt;
                 }
-            }
-        }
-        int idxSize = Integer.MAX_VALUE;
-        for(int i = 0; i < lst.size(); i++){
-            int[] arr = lst.get(i);
-            int size = arr[1] - arr[0];
-            if(size < idxSize) {
-                idxSize = size;
-                answer = arr;
             }
         }
         return answer;
