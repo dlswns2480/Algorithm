f079a0ac57da72fd2eb90d67a68ffccabf7c2289
최인준 <54973090+dlswns2480@users.noreply.github.com>
2025-02-04 04:08:04 +0900

    [Silver II] Title: 나무 자르기, Time: 1916 ms, Memory: 321348 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,47 +1,44 @@
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    static int prevPercent;
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int m = in.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        long answer = 0;
+        int n = sc.nextInt();
+        long m = sc.nextInt();
         int[] arr = new int[n];
-        int answer = 0;
-        for(int i = 0; i < n; i++){
-            arr[i] = in.nextInt();
+        for(int i = 0; i < n; i++) {
+            int height = sc.nextInt();
+            arr[i] = height;
         }
         Arrays.sort(arr);
-        int lt = 1;
-        int rt = arr[arr.length - 1] - 1;
-
+        long lt = 1;
+        long rt = arr[arr.length - 1] - 1;
         while(lt <= rt) {
-            int mid = (lt + rt) / 2;
-            long tree = height(mid, arr);
+            long mid = (lt + rt) / 2;
+            long sum = 0;
+            for(int i = 0; i < arr.length; i++) {
+                long diff = arr[i] - mid;
+                if(diff > 0) sum += diff;
+            }
 
-            if(tree >= m) {
+            if(sum >= m) {
                 lt = mid + 1;
                 answer = mid;
-            }
-            else{
+            } else {
                 rt = mid - 1;
             }
         }
         System.out.print(answer);
-
-    }
-
-    private static long height(int mid, int[] arr){
-        long count = 0;
-        for(int num : arr) {
-            if(mid >= num) continue;
-            count += (num - mid);
-        }
-        return count;
     }
 
 
 }
+
