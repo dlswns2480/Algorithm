d89e96aad24e6d154b3a06f5e523d29a2d46ea2c
최인준 <54973090+dlswns2480@users.noreply.github.com>
2025-02-07 14:19:47 +0900

    [Gold V] Title: A와 B, Time: 152 ms, Memory: 17504 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.java
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -1,31 +1,53 @@
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        String t = sc.next();
-        StringBuilder sb = new StringBuilder(s);
-        StringBuilder tb = new StringBuilder(t);
+    static class Node implements Comparable<Node> {
+        int paper, inter;
+        public Node(int paper, int inter) {
+            this.paper = paper;
+            this.inter = inter;
+        }
 
-        while(sb.length() < tb.length()){
-            if(tb.charAt(tb.length() - 1) == 'A'){
-                tb.deleteCharAt(tb.length() - 1);
+        @Override
+        public int compareTo(Node o) {
+            return this.paper - o.paper;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int answer = 0;
+        String s = br.readLine();
+        String t = br.readLine();
+        for(int i = t.length() - 1; i >= 0; i--) {
+            if(t.charAt(i) == 'A') {
+                t = t.substring(0, i);
+            } else {
+                t = t.substring(0, i);
+                t = reverse(t);
             }
-            else{
-                tb.reverse();
-                tb.deleteCharAt(0);
+            if(t.equals(s)) {
+                answer = 1;
+                break;
             }
         }
-        if (sb.toString().equals(tb.toString())) {
-            System.out.print(1);
-        }
-        else{
-            System.out.print(0);
-        }
+        System.out.print(answer);
+
 
     }
+
+    private static String reverse(String str) {
+        StringBuilder reversed = new StringBuilder();
+        for(int i = str.length() - 1; i >= 0; i--) {
+            reversed.append(str.charAt(i));
+        }
+        return reversed.toString();
+    }
+
+
 }
+
