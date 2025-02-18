f16ed8cdcf7059d701614e31d5323b86b51c8345
최인준 <54973090+dlswns2480@users.noreply.github.com>
2025-02-18 13:41:47 +0900

    [Silver I] Title: 안전 영역, Time: 224 ms, Memory: 18688 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,79 +1,61 @@
 
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[] dx = {1, 0, -1, 0};
+    static int g, s;
+    static int N;
+    static int[][] arr;
+    static int height;
+    static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
-    static int[][] map;
-    static int n;
-    static int[][] dead;
-    static int max = 1;
     static boolean[][] visited;
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        List<Integer> lst = new ArrayList<>();
-        n = Integer.parseInt(br.readLine());
-        map = new int[n][n];
-        dead = new int[n][n];
-        for(int i = 0; i < n; i++){
-            String[] s = br.readLine().split(" ");
-            for(int j = 0; j < n; j++){
-                lst.add(Integer.parseInt(s[j]));
-                map[i][j] = Integer.parseInt(s[j]);
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
+        visited = new boolean[N][N];
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+            for(int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(input[j]);
+                max = Math.max(max, arr[i][j]);
             }
         }
-        for(int i = 1; i <= Collections.max(lst); i++){
-            int result = safe(i);
-            max = Math.max(max, result);
+        int answer = Integer.MIN_VALUE;
+        for(int i = 0; i <= max; i++) {
+            height = i;
+            visited = new boolean[N][N];
+            int count = 0;
+            for(int j = 0; j < N; j++) {
+                for(int k = 0; k < N; k++) {
+                    if(visited[j][k]) continue;
+                    if(arr[j][k] > height) {
+                        count++;
+                        dfs(j, k);
+                    }
+                }
+            }
+            answer = Math.max(answer, count);
         }
-        System.out.print(max);
-
+        System.out.print(answer);
     }
 
-    private static int safe(int rain) {
-        int cnt = 0;
-        visited = new boolean[n][n];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(map[i][j] <= rain){
-                    dead[i][j] = -1;
-                }
-                else{
-                    dead[i][j] = 1;
-                }
-            }
-        }
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(dead[i][j] == 1 && !visited[i][j]){
-                    cnt++;
-                    dfs(i, j);
-                }
-            }
-        }
-        return cnt;
-
-    }
-
-    private static void dfs(int x, int y) {
-        visited[x][y] = true;
-        for(int i = 0; i < 4; i++){
-            int nowX = x + dx[i];
-            int nowY = y + dy[i];
-            if(nowX < 0 || nowY < 0 || nowX >= n || nowY >= n){
-                continue;
-            }
-            if(dead[nowX][nowY] == 1 && !visited[nowX][nowY]){
-                dfs(nowX, nowY);
+    private static void dfs(int y, int x) {
+        if(visited[y][x]) return;
+        visited[y][x] = true;
+        for(int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny < 0 || ny >= N || nx < 0 || nx >= N) continue;
+            if(arr[ny][nx] > height) {
+                dfs(ny, nx);
             }
         }
     }
 
 
 }
+
