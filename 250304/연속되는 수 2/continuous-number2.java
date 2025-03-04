c7af9748e282b4ea880105115dbabdbaf1945ad3
최인준 <54973090+dlswns2480@users.noreply.github.com>
2025-03-04 22:30:47 +0900

    Title: 연속되는 수 2, Time: 170ms, Memory: 12MB, Status: Passed - CodeTree

--- 250304/연속되는 수 2/continuous-number2.java
+++ 250304/연속되는 수 2/continuous-number2.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,11 +9,11 @@ public class Main {
             arr[i] = sc.nextInt();
         }
         int max = Integer.MIN_VALUE;
-        int cnt = 0;
+        int cnt = 1;
         for(int i = 0; i < n; i++) {
             if(i == 0 || arr[i] != arr[i - 1]) {
-                max = Math.max(max, ++cnt);
-                cnt = 0;
+                max = Math.max(max, cnt);
+                cnt = 1;
                 continue;
             }
             cnt++;
