6e097d235cfdbe98bb00bd2402814e3ae028b46b
최인준 <54973090+dlswns2480@users.noreply.github.com>
2025-03-15 00:14:05 +0900

    Title: 이상한 진수 2, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 250315/이상한 진수 2/awkward-digits-2.java
+++ 250315/이상한 진수 2/awkward-digits-2.java
@@ -1,30 +1,30 @@
 import java.util.Scanner;
+
 public class Main {
+    public static final int INT_MIN = Integer.MIN_VALUE;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        char[] input = a.toCharArray();
-        int len = input.length;
-        int max = Integer.MIN_VALUE;
-        for(int i = 0; i < len; i++) {
-            boolean changed = false;
-            if(input[i] == '0') {
-                input[i] = '1';
-                changed = true;
-            }
-            int sum = 0;
-            int idx = 0;
-            for(int j = len - 1; j >= 0; j--) {
-                String str = String.valueOf(input[j]);
-                int toInt = Integer.parseInt(str);
-                sum += (Math.pow(2, idx) * toInt);
-                idx++;
-            }
-            if(changed) input[i] = '0';
-            max = Math.max(max, sum);
+        // 변수 선언 및 입력
+        String binary = sc.next();
+        
+        // 각 i번째 자릿수를 바꾸었을 때의 십진수 값을 구해줍니다.
+        int ans = INT_MIN;
+        for(int i = 0; i < (int) binary.length(); i++) {
+            // i번째 자릿수를 바꿉니다.
+            binary = binary.substring(0, i) + (char)('0' + '1' - binary.charAt(i)) + binary.substring(i + 1);
+            // 십진수로 변환합니다.
+            int num = 0;
+            for(int j = 0; j < (int) binary.length(); j++)
+                num = num * 2 + (binary.charAt(j) - '0');
+            
+            // 가능한 십진수 값 중 최댓값을 구해줍니다.
+            ans = Math.max(ans, num);
+            
+            // i번째 자릿수를 원래대로 돌려놓습니다.
+            binary = binary.substring(0, i) + (char)('0' + '1' - binary.charAt(i)) + binary.substring(i + 1);
         }
-
-        System.out.print(max);
-        // Please write your code here.
+        
+        // 출력
+        System.out.print(ans);
     }
 }
