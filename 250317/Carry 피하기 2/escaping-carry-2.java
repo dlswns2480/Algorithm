1e3e7d0e564fbed8b955a6db92ec093f2be4a100
최인준 <54973090+dlswns2480@users.noreply.github.com>
2025-03-17 01:09:45 +0900

    Title: Carry 피하기 2, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250317/Carry 피하기 2/escaping-carry-2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int answer = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                for(int k = 0; k < n; k++) {
+                    if(i != j && j != k && i != k) {
+                        boolean carry = false;
+                        int first = arr[i];
+                        int sec = arr[j];
+                        int third = arr[k];
+                        int sum = first + sec + third;
+                        if(first % 10 + sec % 10 + third % 10 >= 10) carry = true;
+                        if(first % 100 + sec % 100 + third % 100 >= 100) carry = true;
+                        if(first % 1000 + sec % 1000 + third % 1000 >= 1000) carry = true;
+                        if(first % 10000 + sec % 10000 + third % 10000 >= 10000) carry = true;
+                        if(!carry) answer = Math.max(answer, sum);
+                    }
+                }
+            }
+        }
+        System.out.print(answer);
+        // Please write your code here.
+    }
+}
