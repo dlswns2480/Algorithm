43afaa2f55e849d96932e2bc6a857533f0d07ff2
최인준 <54973090+dlswns2480@users.noreply.github.com>
2025-03-24 14:51:14 +0900

    Title: 행복한 수열의 개수, Time: 145ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250324/행복한 수열의 개수/number-of-happy-sequence.java
+++ 250324/행복한 수열의 개수/number-of-happy-sequence.java
@@ -13,13 +13,11 @@ public class Main {
         int answer = 0;
         for (int i = 0; i < n; i++) {
             int cnt = 1;
-            int prev = grid[i][0];
             for (int j = 1; j < n; j++){
-                if(grid[i][j] == prev) {
+                if(grid[i][j] == grid[i][j- 1]) {
                     cnt++;
                 } else {
                     cnt = 1;
-                    prev = grid[i][j];
                 }
                 if(cnt >= m) {
                     answer++;
@@ -30,13 +28,11 @@ public class Main {
 
         for(int j = 0; j < n; j++) {
             int cnt = 1;
-            int prev = grid[0][j];
             for(int i = 1; i < n; i++) {
-                if(grid[i][j] == prev) {
+                if(grid[i][j] == grid[i - 1][j]) {
                     cnt++;
                 } else {
                     cnt = 1;
-                    prev = grid[i][j];
                 }
                 if(cnt >= m) {
                     answer++;
