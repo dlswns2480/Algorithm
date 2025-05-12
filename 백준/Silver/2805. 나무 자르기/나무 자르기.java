54f97cfbd66daf9315c4a9ac426f9b9e4f0c03e9
최인준 <54973090+dlswns2480@users.noreply.github.com>
2025-05-12 19:15:42 +0900

    [Silver II] Title: 나무 자르기, Time: 512 ms, Memory: 141640 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,44 +1,63 @@
 
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
+    static Map<String, List<String>> dict = new HashMap<>();
+    static List<String> answers = new ArrayList<>();
+    static int n;
+    static Set<String> visited = new HashSet<>();
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Scanner sc = new Scanner(System.in);
-        long answer = 0;
-        int n = sc.nextInt();
-        long m = sc.nextInt();
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++) {
-            int height = sc.nextInt();
-            arr[i] = height;
+        String[] nm = br.readLine().split(" ");
+        int n = Integer.parseInt(nm[0]);
+        long m = Long.parseLong(nm[1]);
+        long[] trees = new long[n];
+        String[] input = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            trees[i] = Long.parseLong(input[i]);
         }
-        Arrays.sort(arr);
-        long lt = 1;
-        long rt = arr[arr.length - 1] - 1;
+
+        long lt = 0;
+        long rt = Arrays.stream(trees).max().getAsLong();
+        long answer = 0;
         while(lt <= rt) {
             long mid = (lt + rt) / 2;
-            long sum = 0;
-            for(int i = 0; i < arr.length; i++) {
-                long diff = arr[i] - mid;
-                if(diff > 0) sum += diff;
-            }
-
-            if(sum >= m) {
+            long height = getHeight(trees, mid);
+            if(height >= m) {
                 lt = mid + 1;
                 answer = mid;
-            } else {
+            } else{
                 rt = mid - 1;
             }
         }
+
         System.out.print(answer);
+
+    }
+
+    private static long getHeight(long[] trees, long mid) {
+        long sum = 0;
+        for(int i = 0; i < trees.length; i++) {
+            if(trees[i] - mid < 0) {
+                continue;
+            }
+            sum += trees[i] - mid;
+        }
+        return sum;
     }
 
 
 }
-
