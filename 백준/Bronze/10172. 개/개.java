d3264956f2d88b02e8bd1a77b37332812e6343b3
최인준 <54973090+dlswns2480@users.noreply.github.com>
2025-08-10 22:51:20 +0900

    [Bronze V] Title: 개, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,15 @@
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
