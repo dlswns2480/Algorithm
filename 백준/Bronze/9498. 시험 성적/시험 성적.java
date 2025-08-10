cbe40f10a75256d2abd165d4f5151c23f4bffe3d
최인준 <54973090+dlswns2480@users.noreply.github.com>
2025-08-10 22:47:58 +0900

    [Bronze V] Title: 시험 성적, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,25 @@
+
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int score = Integer.parseInt(br.readLine());
+        char grade;
+        if (score >= 90) {
+            grade = 'A';
+        } else if (score >= 80) {
+            grade = 'B';
+        } else if (score >= 70) {
+            grade = 'C';
+        } else if (score >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+        System.out.print(grade);
+    }
+}
