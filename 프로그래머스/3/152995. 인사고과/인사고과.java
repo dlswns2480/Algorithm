cb9e6efc7d9768f725754f6404b73cea32a2309d
최인준 <54973090+dlswns2480@users.noreply.github.com>
2025-08-22 17:04:48 +0900

    [level 3] Title: 인사고과, Time: 63.72 ms, Memory: 126 MB -BaekjoonHub

---
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -0,0 +1,61 @@
+import java.util.*;
+class Solution {
+    static class Info implements Comparable<Info> {
+        int attitude, eval;
+        Info(int attitude, int eval) {
+            this.attitude = attitude;
+            this.eval = eval;
+        }
+        
+        public int getSum() {
+            return this.attitude + this.eval;
+        }
+        
+        @Override
+        public int compareTo(Info o) {
+            if (this.attitude == o.attitude) return this.eval - o.eval;
+            return o.attitude - this.attitude;
+        }
+    }
+    public int solution(int[][] scores) {
+        int answer = 0;
+        
+        int wanAttitude = scores[0][0];
+        int wanEval = scores[0][1];
+        
+        List<Info> infos = new ArrayList<>();
+        
+        for (int i = 1; i < scores.length; i++) {
+            infos.add(new Info(scores[i][0], scores[i][1]));
+        }
+        
+        Collections.sort(infos);
+        List<Info> valid = new ArrayList<>();
+        
+        int maxEval = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < infos.size(); i++) {
+            Info curr = infos.get(i);
+            if (curr.eval < maxEval) {
+                
+            } else {
+                valid.add(curr);
+                maxEval = Math.max(maxEval, curr.eval);
+            }
+        }
+        
+        int peopleCount = 0;
+        for (int i = 0; i < valid.size(); i++) {
+            Info info = valid.get(i);
+            if (info.attitude > wanAttitude && info.eval > wanEval) return - 1;
+            if (info.attitude < wanAttitude && info.eval < wanEval) continue;
+            
+            int otherSum = info.getSum();
+            int wanSum = wanAttitude + wanEval;
+            
+            if (otherSum > wanSum) peopleCount++;
+        }
+        
+        return peopleCount + 1;
+    }
+}
