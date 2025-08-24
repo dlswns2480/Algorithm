61f4e546fda8c7a88ad28fdc6d4c21cde11e6f6a
최인준 <54973090+dlswns2480@users.noreply.github.com>
2025-08-24 23:22:51 +0900

    [Bronze V] Title: We love kriii, Time: 96 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,14 @@
+
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("강한친구 대한육군");
+        System.out.print("강한친구 대한육군");
+    }
+}
