462379f2ecc99327b4a78ad9ac45fa4420412779
최인준 <54973090+dlswns2480@users.noreply.github.com>
2025-08-27 13:46:13 +0900

    [Bronze V] Title: A/B, Time: 104 ms, Memory: 14384 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,17 @@
+
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        double A = Double.parseDouble(input[0]);
+        double B = Double.parseDouble(input[1]);
+        System.out.println(A / B);
+
+    }
+}
