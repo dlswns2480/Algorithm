dfaa90d71449fdd8a59f05a54327f4456a6ece3d
최인준 <54973090+dlswns2480@users.noreply.github.com>
2025-08-28 22:25:02 +0900

    [Bronze V] Title: 등록, Time: 96 ms, Memory: 13980 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(215);
+        System.out.println("dlswns6610");
+
+    }
+}
